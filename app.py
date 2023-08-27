df5bf62bb97228cdca262523e6693842af190044
glennwanjiru <glennnjoroge@gmail.com>
2023-08-28 02:40:23 +0300

    Update app.py

--- app.py
+++ app.py
@@ -245,6 +245,7 @@ def main():
 
     st.markdown('<div style="text-align: center;">'
                 '<h2>Contact Information</h2>'
+                
                 '</div>', unsafe_allow_html=True)
 
     # Template for Contacts
